31f50ef888f54b685cdd08f276255f4ad1a25984
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-10-23 10:58:18 +0200

    Bucles Forç

---
+++ Programación/Bucles/Bucles For/PrimerEj_Bucles.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	for (int i = 1; i<=10; i++){
+	
+		printf("%d\n", i);
+	}
+	
+	return EXIT_SUCCESS;
+}
+
---
+++ Programación/Bucles/Bucles For/SegundoEj_Bucles.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	for (int i = 0; i<=21; i+=2){
+		printf("%d\n", i);
+	if(i>20)
+		printf(";");
+
+	}
+	
+	return EXIT_SUCCESS;
+}
+
