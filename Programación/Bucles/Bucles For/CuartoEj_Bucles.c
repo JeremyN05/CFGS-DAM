0203885b7dd63119a1acfcbc037f44da8cc706a0
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-10-23 11:21:31 +0200

    Cuarto ejercicio bucles

---
+++ Programación/Bucles/Bucles For/CuartoEj_Bucles.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){0
+	for (int i = 1; i<=10; i++){
+		
+	   int multi= 5 * i;
+	      printf("La tabla del 5 de todos los números es: %d\n", multi);
+
+	}
+	
+	return EXIT_SUCCESS;
+}
+
