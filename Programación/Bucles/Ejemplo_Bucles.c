0c471ca175a477b7b228e7cffa9140fe120c0dff
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-10-23 12:12:29 +0200

    Ejemplo Bucle For

---
+++ Programación/Bucles/Ejemplo_Bucles.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int i = 0;
+
+for ( 1 = 0 ; 1 = 1 + 1 ; i = 1 + 1){
+	
+/*valor inicial*/ /*condicion*/ /*Codigo a ejecutar entre vueltas*/
+
+	printf("%d", 1);
+
+	/** 
+	 * 1) Inicializo 1=0
+	 * 2) Compruebo si i<5. Como se cumple, ejecuto el interior del bucle.
+	 * 3) Printf
+	 * 4) Terminando el interior del bucle, ejecuto el "código entre vueltas"; 1 = 1 + 1
+	 * 5) Vuelvo al punto 2. Compruebo si i<5. Se cumple porque i==1. Ejecuto el interior.
+	 * 6) Printf
+	 * 7) Terminando el interior del bucle, ejecuto: 1 = 1 + 1. Ahora 1 vale 2.
+	 * 8) Vuelvo a la condicion: ¿i<5? Si. Interior del bucle.
+	 * 9) Printf
+	 * 10) Terminando el interior del bucle, ejecuto 1 = 1 + 1. Ahora vale 3-
+	 * 11) Comprobar la condicion. ¿1<5? Si. Interior.
+	 * 12) 1 vale 4
+	 * 13) 1 vale 4 
+	 * 14) Condicion 
+	 * 15) Condicion
+	 * 16)
+	 * */
+}
+
+return 0;
+
+}
