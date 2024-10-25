9534660dbd4ed357da7638f30d22b4d7491e8029
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-10-25 13:30:59 +0200

    Bucle WhileFor

---
+++ Programación/Bucles/Bucles While/Ejemplo_WhileFor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int i = 0;
+
+	for ( 1 = 0 ; 1 = 1 + 1 ; i = 1 + 1){
+		printf("%d", 1);
+	}
+//Traducimos a while
+
+i= 0;
+
+	while(i<5){ // Condición
+	printf("%d,", i); //El interior de for
+	i = i + 1; //Código entre vueltas
+	
+	}
+	
+	printf("Terminado el while\n");
+	return 0;
+}
+
+
