523ff06cfb04b0c161bd3d2630ad3e8a48261ad4
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-10-30 10:08:25 +0100

    Ejercicio 5 Bucles While

---
+++ Programación/Bucles/Bucles While/5Ej_Bucles.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int r;
+
+	while(1){ 
+		r= rand();
+		//Hacer módulo ocho da números aleatorios entre 0 y 6
+		printf("%d\n", r);
+	
+	}
+
+	return EXIT_SUCCESS;
+
+}
