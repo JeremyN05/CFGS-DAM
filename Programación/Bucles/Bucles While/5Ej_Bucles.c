fed102c55c929cffe7ca366f6289404ee2dd99f3
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-10-30 10:17:55 +0100

    Actualización ejercicio 5 Bucles While

--- Programación/Bucles/Bucles While/5Ej_Bucles.c
+++ Programación/Bucles/Bucles While/5Ej_Bucles.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
 int r;
 
-	while(1){ 
-		r= rand();
+	srand(time(NULL)); //Cada segundo
+	//srand(getpid()); //Identificador del proceso 
+	
+		for (int i = 0; i < 4; i++);
+		// rand() % 5 genera números aleatorio entre 0 y 4 incluidos. Al sumar 1, consigo números aleatorios entre 1 y 5.
+		r = (rand() % 5) + 1;
 		//Hacer módulo ocho da números aleatorios entre 0 y 6
 		printf("%d\n", r);
-	
-	}
 
 	return EXIT_SUCCESS;
 
 }
+
+/* Construir una función que calcule un número aleatorio entre dos valores dados como argumento*/
