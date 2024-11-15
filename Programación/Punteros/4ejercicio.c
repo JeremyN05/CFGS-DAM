657f24b955bb8e1d4457b33f4758aedc64eb83d1
JeremyN05 <narvaezlobatojeremy@gmail.com>
2024-11-15 13:16:05 +0100

    Ejercicio 4 V2

--- Programación/Punteros/4ejercicio.c
+++ Programación/Punteros/4ejercicio.c
@@ -4,15 +4,18 @@
 //Ejemplo de paso de un argumento a una función por valor
 //Quiero que realmente incremente la variable dada como argumento
 //
-void intercambiar(int *n1, int *n2){
-
+void intercambiar(int *n1, int *n2, int *n3){
+	*n3 = *n2;
+	*n2 = *n1;
+	*n1 = *n3;
 }
 
 int main(){
 	int n1 = 10;
 	int n2 = 30;
-	
-	intercambiar(&n1, &n2); // le estoy dando el valor 10, no la variable
+	int n3 = 0;
+
+	intercambiar(&n1, &n2, &n3); // le estoy dando el valor 10, no la variable
 
 	printf("%d\n", n1);
 	printf("%d\n", n2);
