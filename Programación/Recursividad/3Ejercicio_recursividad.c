a71021cc86b17db00a2b35d3e79a93f438704123
JeremyN05 <narvaezlobatojeremy@gmail.com>
2025-02-11 13:13:08 +0100

    Ejercicio 3 v1

---
+++ Programación/Recursividad/3Ejercicio_recursividad.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char imprimir_vocales(char * vocales, int palabra){
+	
+	if(palabra == 0)
+}
+
+int main(){
+
+	char palabra[50];
+
+	printf("Introuzca la palabra: ");
+	scanf("%s", palabra);
+
+	printf("la palabra es: %s \n", palabra);
+
+	return 0;
+}
